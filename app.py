e3da4cc4d354fb7099230fd668028ed8282342de
saketh <sakethdontha2001@gmail.com>
2025-10-31 15:30:19 -0500

    changes made

--- app.py
+++ app.py
@@ -25,39 +25,6 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # ----------------------------
 st.set_page_config(page_title="University of Memphis — University Contact Information Form", page_icon="🎓", layout="centered")
 
-hide_streamlit_style = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stStatusWidget"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                #MainMenu {
-                visibility: hidden;
-                height: 0%;
-                }
-                header {
-                visibility: hidden;
-                height: 0%;
-                }
-                footer {
-                visibility: hidden;
-                height: 0%;
-                }
-                </style>
-                """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 st.markdown(
     """
     <style>
